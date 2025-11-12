7063a57a8b9713ce3ffc6a542af52f929c305118
vathana <siengvathana1@gmail.com>
2025-11-12 10:45:34 +0700

    uppdate

--- app/provider/GoogleMapsProvider.tsx
+++ app/provider/GoogleMapsProvider.tsx
@@ -8,7 +8,7 @@ const GOOGLE_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY;
 
 export default function GoogleMapsProvider({ children }: { children: ReactNode }) {
   return (
-    <LoadScript googleMapsApiKey={GOOGLE_API_KEY} libraries={["places"]}>
+    <LoadScript googleMapsApiKey={GOOGLE_API_KEY!} libraries={["places"]}>
       {children}
     </LoadScript>
   );
